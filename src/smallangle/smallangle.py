a1ee95814fa42447f64a0464a3b8ed4215072051
ECPCsam <samtenbroek@gmail.com>
2024-11-15 14:49:47 +0100

    Update smallangle.py
    added click and docstrings

--- src/smallangle/smallangle.py
+++ src/smallangle/smallangle.py
@@ -4,17 +4,46 @@ from numpy import pi
 import pandas as pd
 
 
+@click.group()
+def cmd_group():
+    """Initializes the cmd_group click group
+    """
+    pass
+
+@cmd_group.command()
+@click.option(
+    "--number",
+    "-n",
+    default = 10,
+    help = "Amount of numbers printed",
+    show_default = True,
+)
 def sin(number):
-    x = np.linspace(0, 2 * pi, number)
+    """Display numbers and their sinus values as if they were radians.
+
+    NUMBER gives the amount of numbers.
+    """    
+    x = np.linspace(0, 2 * pi, int(number))
     df = pd.DataFrame({"x": x, "sin (x)": np.sin(x)})
     print(df)
 
-
+@cmd_group.command()
+@click.option(
+    "--number",
+    "-n",
+    default = 10,
+    help = "Amount of numbers printed",
+    show_default = True,
+)
 def tan(number):
-    x = np.linspace(0, 2 * pi, number)
+    """Display numbers and their tangent values as if they were radians.
+
+    NUMBER gives the amount of numbers shown.
+    """
+    x = np.linspace(0, 2 * pi, int(number))
     df = pd.DataFrame({"x": x, "tan (x)": np.tan(x)})
     print(df)
 
 
 if __name__ == "__main__":
-    sin(10)
+    cmd_group()
