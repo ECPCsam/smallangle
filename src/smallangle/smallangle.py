a81bfaa896711df72b8da56b28cf2a3a4ff0787b
ECPCsam <samtenbroek@gmail.com>
2024-11-15 15:41:40 +0100

    Update smallangle.py
    Added the option to use -h for help, and improved docstrings.

--- src/smallangle/smallangle.py
+++ src/smallangle/smallangle.py
@@ -3,10 +3,12 @@ import numpy as np
 from numpy import pi
 import pandas as pd
 
+#context settings, add this to command group to allow '-h' to be used aswell as '--help'
+CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 
-@click.group()
+@click.group(context_settings = CONTEXT_SETTINGS)
 def cmd_group():
-    """Initializes the cmd_group click group
+    """Calculate a certain amount of sinus or tangent values.
     """
     pass
 
